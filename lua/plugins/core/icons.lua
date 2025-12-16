f1ff5bbac6ae027e77b04058620a49d7ccbe1c8b
zhou <zhouzhou.zhang.24@ucl.ac.uk>
2025-12-16 18:01:22 +0800

    Adds default webicons config.

--- lua/plugins/core/icons.lua
+++ lua/plugins/core/icons.lua
@@ -1,84 +1,61 @@
 return {{
     "nvim-tree/nvim-web-devicons",
-    lazy = false,
-    priority = 1000,
+
+    opts = {},
 
     config = function()
-        require("nvim-web-devicons").setup({
+
+        require'nvim-web-devicons'.setup {
+            -- your personal icons can go here (to override)
+            -- you can specify color or cterm_color instead of specifying both of them
+            -- DevIcon will be appended to `name`
             override = {
-                lua = {
-                    icon = "",
-                    color = "#51a0cf",
-                    name = "Lua"
-                },
-                cpp = {
-                    icon = "󰙲",
-                    color = "#4dffe4",
-                    name = "Cpp"
-                },
-                go = {
-                    icon = "󰟓",
-                    color = "#4dffe4",
-                    name = "Go"
-                },
-                java = {
-                    icon = "󰅶",
-                    color = "#ff6d4d",
-                    name = "Java"
-                },
-                python = {
-                    icon = "",
-                    color = "#fffc45",
-                    name = "Python"
-                },
-                md = {
-                    icon = "",
-                    color = "#ffdb87",
-                    name = "Markdown"
+                zsh = {
+                    icon = "",
+                    color = "#428850",
+                    cterm_color = "65",
+                    name = "Zsh"
                 }
             },
-
+            -- globally enable different highlight colors per icon (default to true)
+            -- if set to false all icons will have the default icon's color
             color_icons = true,
-
-            override_by_extension = {
-                ["h"] = {
-                    icon = "",
-                    color = "#ff38b3",
-                    name = "c-header"
-                },
-
-                ["hpp"] = {
-                    icon = "",
-                    color = "#ff38b3",
-                    name = "cpp-header"
-                },
-
-                ["env"] = {
-                    icon = "",
-                    color = "#ecd53f",
-                    name = "Env"
-                }
-
-            },
-
+            -- globally enable default icons (default to false)
+            -- will get overriden by `get_icons` option
+            default = true,
+            -- globally enable "strict" selection of icons - icon will be looked up in
+            -- different tables, first by filename, and if not found by extension; this
+            -- prevents cases when file doesn't have any extension but still gets some icon
+            -- because its name happened to match some extension (default to false)
+            strict = true,
+            -- set the light or dark variant manually, instead of relying on `background`
+            -- (default to nil)
+            variant = "light|dark",
+            -- override blend value for all highlight groups :h highlight-blend.
+            -- setting this value to `0` will make all icons opaque. in practice this means
+            -- that icons width will not be affected by pumblend option (see issue #608)
+            -- (default to nil)
+            blend = 0,
+            -- same as `override` but specifically for overrides by filename
+            -- takes effect when `strict` is true
             override_by_filename = {
-                ["Dockerfile"] = {
-                    icon = "",
-                    color = "#2496ed",
-                    name = "Dockerfile"
-                },
-                ["CMakeLists.txt"] = {
-                    icon = "󱥒",
-                    color = "#3d33ff",
-                    name = "CMakeLists"
-                },
-                ["README.md"] = {
-                    icon = "",
-                    color = "#5759ad",
-                    name = "README"
+                [".gitignore"] = {
+                    icon = "",
+                    color = "#f1502f",
+                    name = "Gitignore"
                 }
             },
-
+            -- same as `override` but specifically for overrides by extension
+            -- takes effect when `strict` is true
+            override_by_extension = {
+                ["log"] = {
+                    icon = "",
+                    color = "#81e043",
+                    name = "Log"
+                }
+            },
+            -- same as `override` but specifically for operating system
+            -- takes effect when `strict` is true
             override_by_operating_system = {
                 ["apple"] = {
                     icon = "",
@@ -86,10 +63,8 @@ return {{
                     cterm_color = "248",
                     name = "Apple"
                 }
-            },
-
-            default = true
-        })
+            }
+        }
 
     end
 }}
