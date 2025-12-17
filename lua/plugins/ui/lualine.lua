285c0f2017e9e424788d3b84b2106ac0f04a1035
zhou <zhouzhou.zhang.24@ucl.ac.uk>
2025-12-17 13:27:37 +0800

    Improves the icon and color scheme.

--- lua/plugins/ui/lualine.lua
+++ lua/plugins/ui/lualine.lua
@@ -1,73 +1,86 @@
-return {{
-    "nvim-lualine/lualine.nvim",
-    dependencies = {"nvim-tree/nvim-web-devicons"},
+return {
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
 
-    config = function()
-        require("lualine").setup({
-            options = {
-                theme = "auto",
-                globalstatus = true,
-                section_separators = {
-                    left = "",
-                    right = ""
-                },
-                component_separators = {
-                    left = "",
-                    right = ""
-                },
-                icons_enabled = true
-            },
+		config = function()
+			require("lualine").setup({
+				options = {
+					theme = "auto",
+					globalstatus = true,
+					section_separators = {
+						left = "",
+						right = "",
+					},
+					component_separators = {
+						left = "",
+						right = "",
+					},
+					icons_enabled = true,
+				},
 
-            sections = {
-                lualine_a = {{
-                    "mode",
-                    icon = ""
-                }},
+				sections = {
+					lualine_a = { {
+						"mode",
+						icon = "",
+					} },
 
-                lualine_b = {{
-                    "branch",
-                    icon = ""
-                }, {
-                    "diff",
-                    symbols = {
-                        added = "",
-                        modified = "󰬂",
-                        removed = ""
-                    }
-                }},
+					lualine_b = {
+						{
+							"branch",
+							icon = "",
+						},
+						{
+							"diff",
+							symbols = {
+								added = "",
+								modified = "",
+								removed = "",
+							},
+						},
+					},
 
-                lualine_c = {{
-                    "filename",
-                    path = 1,
-                    symbols = {
-                        modified = " ●",
-                        readonly = " "
-                    }
-                }},
+					lualine_c = {
+						{
+							"filename",
+							path = 1,
+							symbols = {
+								readonly = " ",
+								modified = "",
+							},
+						},
+					},
 
-                lualine_x = {{
-                    "diagnostics",
-                    sources = {"nvim_lsp"}
-                }, {function()
-                    local clients = vim.lsp.get_active_clients({
-                        bufnr = 0
-                    })
-                    if #clients == 0 then
-                        return ""
-                    end
-                    return " " .. clients[1].name
-                end}, "encoding", "fileformat"},
+					lualine_x = {
+						{
+							"diagnostics",
+							sources = { "nvim_lsp" },
+						},
+						{
+							function()
+								local clients = vim.lsp.get_active_clients({
+									bufnr = 0,
+								})
+								if #clients == 0 then
+									return ""
+								end
+								return " " .. clients[1].name
+							end,
+						},
+						"encoding",
+						"fileformat",
+					},
 
-                lualine_y = {"progress"},
+					lualine_y = { "progress" },
 
-                lualine_z = {"location"}
-            },
+					lualine_z = { "location" },
+				},
 
-            inactive_sections = {
-                lualine_c = {"filename"},
-                lualine_x = {"location"}
-            }
-        })
-
-    end
-}}
+				inactive_sections = {
+					lualine_c = { "filename" },
+					lualine_x = { "location" },
+				},
+			})
+		end,
+	},
+}
--- lua/plugins/ui/nvim-tree.lua
+++ lua/plugins/ui/nvim-tree.lua
@@ -1,43 +1,42 @@
 return {
-    "nvim-tree/nvim-tree.lua",
-    dependencies = {"nvim-tree/nvim-web-devicons"},
-    opts = {
-        git = {
-            enable = true,
-            ignore = false,
-            show_on_dirs = true,
-            show_on_open_dirs = false,
-            timeout = 200
-        },
-        diagnostics = {
-            enable = true,
-            show_on_dirs = true,
-            icons = {
-                hint = "",
-                info = "",
-                warning = "",
-                error = ""
-            }
-        },
+	"nvim-tree/nvim-tree.lua",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	opts = {
+		git = {
+			enable = true,
+			ignore = false,
+			show_on_dirs = true,
+			show_on_open_dirs = false,
+			timeout = 200,
+		},
+		diagnostics = {
+			enable = true,
+			show_on_dirs = true,
+			icons = {
+				hint = "",
+				info = "",
+				warning = "",
+				error = "",
+			},
+		},
 
-        renderer = {
-            special_files = {},
-            icons = {
-                show = {
-                    git = true
-                },
-                glyphs = {
-                    git = {
-                        unstaged = "󰬂",
-                        staged = "✓",
-                        unmerged = "",
-                        renamed = "",
-                        untracked = "",
-                        deleted = "",
-                        ignored = ""
-                    }
-                }
-            }
-        }
-    }
+		renderer = {
+			icons = {
+				show = {
+					git = true,
+				},
+				glyphs = {
+					git = {
+						unstaged = "",
+						staged = "󱓟",
+						unmerged = "",
+						renamed = "",
+						untracked = "",
+						deleted = "",
+						ignored = "",
+					},
+				},
+			},
+		},
+	},
 }
