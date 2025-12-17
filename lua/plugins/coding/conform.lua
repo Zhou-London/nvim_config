d226442a90dcaffd574970e754a01a38517ee7ce
zhou <zhouzhou.zhang.24@ucl.ac.uk>
2025-12-17 13:23:38 +0800

    Supports lsp & formatter for Lua.

--- lua/plugins/coding/conform.lua
+++ lua/plugins/coding/conform.lua
@@ -1,11 +1,14 @@
-return {{
-    'stevearc/conform.nvim',
-    opts = {
-        format_on_save = false,
-        formatters_by_ft = {
-            c = {"clang_format"},
-            cpp = {"clang_format"},
-            objc = {"clang_format"}
-        }
-    }
-}}
+return {
+	{
+		"stevearc/conform.nvim",
+		opts = {
+			format_on_save = false,
+			formatters_by_ft = {
+				c = { "clang_format" },
+				cpp = { "clang_format" },
+				objc = { "clang_format" },
+				lua = { "stylua" },
+			},
+		},
+	},
+}
--- lua/plugins/coding/lsp.lua
+++ lua/plugins/coding/lsp.lua
@@ -1,15 +1,36 @@
 return {
-    "neovim/nvim-lspconfig",
-    config = function()
-        local capabilities = require("cmp_nvim_lsp").default_capabilities()
+	"neovim/nvim-lspconfig",
+	config = function()
+		local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-        vim.lsp.config["clangd"] = {
-            cmd = {"clangd", "--background-index", "--clang-tidy", "--completion-style=detailed",
-                   "--header-insertion=iwyu"},
-            filetypes = {"c", "cc", "cpp"},
-            capabilities = capabilities
-        }
+		vim.lsp.config["clangd"] = {
+			cmd = {
+				"clangd",
+				"--background-index",
+				"--clang-tidy",
+				"--completion-style=detailed",
+				"--header-insertion=iwyu",
+			},
+			filetypes = { "c", "cc", "cpp" },
+			capabilities = capabilities,
+		}
 
-        vim.lsp.enable("clangd")
-    end
+		vim.lsp.config["lua_ls"] = {
+			cmd = { "lua-language-server" },
+			filetypes = { "lua" },
+			root_markers = { { ".luarc.json", ".luarc.jsonc" }, ".git" },
+			capabilities = capabilities,
+
+			settings = {
+				Lua = {
+					runtime = {
+						version = "LuaJIT",
+					},
+				},
+			},
+		}
+
+		vim.lsp.enable("clangd")
+		vim.lsp.enable("lua_ls")
+	end,
 }
