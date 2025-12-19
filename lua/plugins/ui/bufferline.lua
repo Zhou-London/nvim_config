2d1c286d1dd26567fb22fc9a2a2d3a0ad5c05dd6
zhou <zhouzhou.zhang.24@ucl.ac.uk>
2025-12-19 18:15:12 +0800

    Fixs bufferline close issue.

--- lua/plugins/ui/bufferline.lua
+++ lua/plugins/ui/bufferline.lua
@@ -1,53 +1,41 @@
 return {
-	{
-		"akinsho/bufferline.nvim",
-		version = "*",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		event = "VeryLazy",
-		opts = {
-			options = {
-				mode = "buffers", -- buffers | tabs
-				numbers = "none", -- none | ordinal | buffer_id
-				indicator = {
-					style = "icon",
-					icon = "┋",
-				},
-				separator_style = "thin", -- slant | padded_slant | slope | thin | thick
-				diagnostics = "nvim_lsp",
-				diagnostics_indicator = function(count, level)
-					local icon = level:match("error") and " " or ""
-					return icon .. count
-				end,
-				modified_icon = "󰰐",
-				color_icons = true,
-				offsets = {
-					{
-						filetype = "NvimTree",
-						text = "Explorer",
-						highlight = "Directory",
-						separator = false,
+  {
+    "akinsho/bufferline.nvim",
+    version = "*",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+      "famiu/bufdelete.nvim",
+    },
+    config = function()
+      local bufferline = require("bufferline")
 
-					},
-				},
-				show_buffer_close_icons = true,
-				show_close_icon = false,
-				always_show_bufferline = true,
+      bufferline.setup({
+        options = {
+          mode = "buffers",
+          style_preset = bufferline.style_preset.minimal,
+          numbers = "none",
+          diagnostics = "nvim_lsp",
 
-				highlights = {
-					buffer_selected = {
-						fg = "#ffffff",
-						bg = "#1f2335",
-						bold = true,
-					},
-					buffer_visible = {
-						fg = "#a9b1d6",
-						bg = "#1a1b26",
-					},
-					fill = {
-						bg = "#16161e",
-					},
-				},
-			},
-		},
-	},
+          offsets = {
+            {
+              filetype = "NvimTree",
+              text = "File Explorer",
+              text_align = "left",
+              separator = false,
+            },
+          },
+
+          close_command = function(bufnum)
+            require("bufdelete").bufdelete(bufnum, true)
+          end,
+
+          right_mouse_command = function(bufnum)
+            require("bufdelete").bufdelete(bufnum, true)
+          end,
+
+          color_icons = true,
+        },
+      })
+    end,
+  },
 }
